4eb5dec3c73ceaa32e47838ce3d5665ee1cb2b1a
karolis-KK <karkis315@gmail.com>
2025-01-17 19:01:20 +0200

    dates

--- app/components/Tag.jsx
+++ app/components/Tag.jsx
@@ -2,6 +2,13 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Tag(props) {
+  const monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+
+  const date = new Date(); // Get the current date
+  const month = date.getMonth(); 
   return (
     <div
       className={`${inter.className} flex items-center absolute bottom-12 left-12 lg:left-24 gap-x-12 lg:gap-x-24 text-xs`}
@@ -10,7 +17,7 @@ export default function Tag(props) {
         <>
           <h1 className="text-neutral-200">wearware / ai</h1>
           <h1 className="text-neutral-200 hidden lg:block">Reviews / Mobile Strategy</h1>
-          <h1 className="text-white/55">December {new Date().getFullYear()}</h1>
+          <h1 className="text-white/55">{monthNames[month]} {new Date().getFullYear()}</h1>
           <h1 className="text-white/55">{props.page}</h1>
         </>
       ) : (
@@ -18,7 +25,7 @@ export default function Tag(props) {
           <h1 className="text-neutral-900">wearware / ai</h1>
           <h1 className="text-neutral-900 hidden lg:block">Reviews / Mobile Strategy</h1>
           <h1 className="text-neutral-900/55">
-            December {new Date().getFullYear()}
+            {monthNames[month]} {new Date().getFullYear()}
           </h1>
           <h1 className="text-neutral-900/55">{props.page}</h1>
         </>
